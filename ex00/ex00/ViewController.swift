04b16cd4e4e0ee65672f78b3433576503da69995
SergeyOnline <SergeyOnline@protonmail.com>
2021-06-20 16:53:02 +0300

    create vc

---
+++ ex00/ex00/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Shira Broderick on 6/20/21.
+//  Copyright © 2021 Shira Broderick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+	
+	var collectionView: UICollectionView!
+	let id = "Cell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		let flowLayout = UICollectionViewFlowLayout()
+		collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: flowLayout)
+		collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: id)
+		collectionView.delegate = self
+		collectionView.dataSource = self
+		collectionView.backgroundColor = .systemGray4
+		
+		self.view.addSubview(collectionView)
+		
+
+    }
+    
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		4
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: id, for: indexPath)
+		cell.backgroundColor = .green
+		cell.autoresizingMask = .init(arrayLiteral: .flexibleHeight, .flexibleWidth)
+		return cell
+		
+	}
+	
+//	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//		let size = self.view.bounds.width / 2 - 10
+//		return CGSize(width: size, height: size)
+//	}
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+		return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+	}
+
+}
